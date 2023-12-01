25e4c5f0eae3737db69784c104d5914c00856444
KYI <headwaving@gmail.com>
2023-12-01 22:49:26 +0900

    Remove test hooks

--- app/lib/hooks.tsx
+++
@@ -1,8 +0,0 @@
-import useSWR from 'swr';
-
-const fetcher = (args: any) => fetch(args).then((res) => res.text());
-
-export function useUser() {
-  const { data, error } = useSWR('http://cola053.asuscomm.com:4000/test', fetcher);
-  return error ? null : data;
-}
